1e1ba561e552fcecd7c5728acca786bdfc263606
minjeongkk <kunvx@naver.com>
2023-03-15 22:16:04 +0900

    bfs

--- dfs_bfs/5-9.py
+++ dfs_bfs/5-9.py
@@ -21,9 +21,6 @@ def bfs(graph, start, visited):
                 queue.append(i)
                 visited[i] = True
 
-
-
-
 # 각 노드가 연결된 정보를 리스트 자료형으로 표현(2차원 리스트)
 graph = [
     [],
@@ -36,7 +33,6 @@ graph = [
     [2,6,8],
     [1,7]
 ]
-
 
 # 각 노드가 방문된 정보를 리스트 자료형으로 표현(1차원 리스트)
 visited = [False] * 9
